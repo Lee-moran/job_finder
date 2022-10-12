9ede15bb9e56856a6d1c4f060e461d91338ed48b
Lee-moran <leemoran06@hotmail.com>
2022-10-12 13:42:48 +0000

    installed react-router, and bootstrap container

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/finder.png";
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
-          <img src={logo} alt="logo" height="50" />
+          <img src={logo} alt="logo" height="60"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <Nav.Link>
               <i className="fas fa-house-user"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-circle-info"></i>About
             </Nav.Link>
             <Nav.Link>
               <i className="fas fa-sign-in-alt"></i>Sign in
