7472d646e289fea2ac023e65e46b4948d3844a50
Lee-moran <leemoran06@hotmail.com>
2022-10-25 12:57:53 +0000

    MoreDrop down on profile, alows you to edit etc

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import { useCurrentUser } from './contexts/CurrentUserContext';
 import About from './pages/posts/About';
 import PostEditForm from './pages/posts/PostEditForm';
 import ProfilePage from './pages/profiles/ProfilePage';
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 
 
@@ -62,6 +65,9 @@ function App() {
               <Route exact path="/about" render={() => <About/> } />
               <Route exact path="/posts/:id/edit" render={() => <PostEditForm/>}/>
               <Route exact path="/profiles/:id" render={() => <ProfilePage/>}/>
+              <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
+              <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />}/>
+              <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />}/>
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
