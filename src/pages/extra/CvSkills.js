497f7e54a1386ecbb732dd45a2b5bb5515aa1dde
Lee-moran <leemoran06@hotmail.com>
2022-10-28 14:57:14 +0000

    Cvskills more added

--- src/pages/extra/CvSkills.js
+++ src/pages/extra/CvSkills.js
@@ -1,9 +1,71 @@
 import React from 'react'
+import {Card, ListGroup, Container, Row, Col,}from 'react-bootstrap';
+import cv from '../../assets/Cv.jpg';
+import TEMP from '../../assets/CVTEMP.jpg';
+import group from '../../assets/group.jpg';
+import styles from '../../styles/CV.module.css';
 
-const CvSkills = () => {
+
+const CvSkills = () => { 
   return (
-    <div>CvSkills</div>
-  )
+    <Container className={styles.TOP}>
+      <Row>
+        <Col xs={{ order: 'last' }}>
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={cv} />
+            <Card.Body>
+              <Card.Title className={styles.head}>CV Skills</Card.Title>
+              <Card.Text className={styles.body} >
+                Some quick tip to improve your CV
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item className={styles.body}>Masters in recurting</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link className={styles.link} href="https://www.hays.ie/resources/career-advice/writing-a-cv"> HAYS</Card.Link>
+            </Card.Body>
+          </Card>
+          </Col>
+        <Col xs>
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={TEMP} />
+            <Card.Body>
+              <Card.Title className={styles.head}>CV Skills</Card.Title>
+              <Card.Text className={styles.body}>
+              Your ultimate guide on how to write a CV
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item className={styles.body}>Masters in recurting</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link className={styles.link} href="https://www.recruiters.ie/blog/how-to-write-a-cv-guide/"> RECRUITERS.IE </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={{ order: 'first' }}>
+        <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={group} />
+            <Card.Body>
+              <Card.Title className={styles.head}>CV Skills</Card.Title>
+              <Card.Text className={styles.body}>
+              Looking for professional Help
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item className={styles.body}> Reach out to our memebers </ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link className={styles.link} href="/"> Home </Card.Link>
+            </Card.Body>
+          </Card>
+          
+          
+          </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default CvSkills
